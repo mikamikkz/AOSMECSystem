ef77c976aab15e58311d32d60c50e68b8782a02d
Vin Myca <vinmycasagarino@gmail.com>
2021-01-15 18:35:02 +0800

    v: editted Navigation

--- aosmec/src/components/Navigation.vue
+++ aosmec/src/components/Navigation.vue
@@ -6,7 +6,6 @@
       width="260"
       app
     >
-    <v-card flat class="not-rounded" color="#13b150">
       <v-list-item class="pb-1">
         <v-list-item-content class="mt-2 ml-2">
           <v-list-item-title style="font-size: 20px" class="title white--text">
@@ -15,57 +14,35 @@
           <v-list-item-subtitle style="color: #e3fff6">Booking System</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
-    </v-card>
       <v-divider></v-divider>
 
-    <v-list dense nav>
+    <v-list dense nav class="px-0 mx-0">
       <v-list-item-group>
         <router-link to="/dashboard" style="color: #c8f0f9">
           <v-list-item color="black">
-            <v-list-item-icon class="ml-2 ma-0 mt-2 mr-2">
+            <v-list-item-icon class="ml-4 ma-0 mt-2 mr-2">
               <v-icon small style="color: #e3fff6">mdi-view-dashboard</v-icon>
             </v-list-item-icon>
-            <v-list-item-content style="color: #e3fff6; font-size: 14px">
+            <v-list-item-content style="color: #e3fff6">
               Dashboard
             </v-list-item-content>
           </v-list-item>
         </router-link>
 
-        <v-list-group color="black">
-          <template v-slot:activator>
-            <v-list-item-icon class="ma-0 mt-2 mr-2 pl-2 pr-2">
-              <v-icon small style="color: #e3fff6">mdi-calendar-clock</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content style="color: #e3fff6; font-size: 14px">
-              Bookings
-            </v-list-item-content>
-          </template>
-
-          <router-link to="/reservation" style="color: #daf5fb">
-            <v-list-item color="black">
-              <v-list-item-icon class="ml-10 ma-0 mt-2">
-                <v-icon small style="color: #e3fff6">mdi-calendar-check</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content style="color: #e3fff6" class="list-item">
-                Reservation
-              </v-list-item-content>
-            </v-list-item>
-          </router-link>
-
-          <v-list-group color="white">
+          <v-list-group color="black">
             <template v-slot:activator>
-                <v-list-item-icon class="ma-0 mt-2 mr-2 pl-2 pr-2">
+                <v-list-item-icon class="ma-0 ml-2 mt-2 mr-2 pl-2 pr-2">
                   <v-icon small style="color: #e3fff6"
                     >mdi-calendar-clock</v-icon
                   >
                 </v-list-item-icon>
-                <v-list-item-content style="color: #e3fff6; font-size: 14px">
+                <v-list-item-content style="color: #e3fff6">
                   Bookings
                 </v-list-item-content>
             </template>
 
             <router-link to="/reservation" style="color: #daf5fb">
-              <v-list-item>
+              <v-list-item color="black">
                 <v-list-item-icon class="ml-10 ma-0 mt-2">
                   <v-icon small style="color: #e3fff6"
                     >mdi-calendar-check</v-icon
@@ -78,7 +55,7 @@
             </router-link>
 
             <router-link to="/checkin" style="color: #daf5fb">
-              <v-list-item>
+              <v-list-item color="black">
                 <v-list-item-icon class="ml-10 ma-0 mt-2">
                   <v-icon small style="color: #e3fff6"
                     >mdi-account-check</v-icon
@@ -91,69 +68,65 @@
             </router-link>
           </v-list-group>
 
-          <v-list-group color="white">
+        <v-list-group color="black">
             <template v-slot:activator>
-              <v-list-item to="/room-mgmt" color="white">
-                <v-list-item-icon class="ma-0 mt-2 mr-2 ml-0 pr-2">
-                  <v-icon small style="color: #e3fff6">mdi-bed</v-icon>
+              <v-list-item to="/room-mgmt/all" color="black">
+                <v-list-item-icon class="ma-0 ml-2 mt-2 mr-2">
+                  <v-icon small style="color: #e3fff6"
+                    >mdi-calendar-clock</v-icon
+                  >
                 </v-list-item-icon>
-                <v-list-item-content style="color: #e3fff6; font-size: 14px">
+                <v-list-item-content style="color: #e3fff6; font-size: 15px;">
                   Room Management
                 </v-list-item-content>
               </v-list-item>
             </template>
 
-        <v-list-group color="black">
-          <template v-slot:activator>
-            <v-list-item-icon class="ml-2 ma-0 mt-2 mr-2">
-              <v-icon small style="color: #edfafd">mdi-bed</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content
-              style="color: #daf5fb; font-size: 14px"
-              to="/room-mgmt"
-            >
-              Room Management
-            </v-list-item-content>
-          </template>
+            <router-link to="/room-mgmt/floor-1" style="color: #daf5fb">
+              <v-list-item color="black">
+                <v-list-item-icon class="ml-10 ma-0 mt-2">
+                  <v-icon small style="color: #e3fff6"
+                    >mdi-floor-plan</v-icon
+                  >
+                </v-list-item-icon>
+                <v-list-item-content style="color: #e3fff6" class="list-item">
+                  Floor 1
+                </v-list-item-content>
+              </v-list-item>
+            </router-link>
 
-          <router-link to="/room-mgmt/floor-1" style="color: #daf5fb">
-            <v-list-item color="black">
-              <v-list-item-icon class="ml-10 ma-0 mt-2">
-                <v-icon small style="color: #e3fff6">mdi-floor-plan</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content style="color: #e3fff6C" class="list-item">
-                Floor 1
-              </v-list-item-content>
-            </v-list-item>
-          </router-link>
-          <router-link to="/room-mgmt/floor-2" style="color: #daf5fb">
-            <v-list-item color="black">
-              <v-list-item-icon class="ml-10 ma-0 mt-2">
-                <v-icon small style="color: #e3fff6">mdi-floor-plan</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content style="color: #e3fff6" class="list-item">
-                Floor 2
-              </v-list-item-content>
-            </v-list-item>
-          </router-link>
-          <router-link to="/room-mgmt/floor-3" style="color: #daf5fb">
-            <v-list-item color="black">
-              <v-list-item-icon class="ml-10 ma-0 mt-2">
-                <v-icon small style="color: #e3fff6">mdi-floor-plan</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content style="color: #e3fff6" class="list-item">
-                Floor 3
-              </v-list-item-content>
-            </v-list-item>
-          </router-link>
-        </v-list-group>
+            <router-link to="/room-mgmt/floor-2" style="color: #daf5fb">
+              <v-list-item color="black">
+                <v-list-item-icon class="ml-10 ma-0 mt-2">
+                  <v-icon small style="color: #e3fff6"
+                    >mdi-floor-plan</v-icon
+                  >
+                </v-list-item-icon>
+                <v-list-item-content style="color: #e3fff6" class="list-item">
+                  Floor 2
+                </v-list-item-content>
+              </v-list-item>
+            </router-link>
+            <router-link to="/room-mgmt/floor-3" style="color: #daf5fb">
+              <v-list-item color="black">
+                <v-list-item-icon class="ml-10 ma-0 mt-2">
+                  <v-icon small style="color: #e3fff6"
+                    >mdi-floor-plan</v-icon
+                  >
+                </v-list-item-icon>
+                <v-list-item-content style="color: #e3fff6" class="list-item">
+                  Floor 3
+                </v-list-item-content>
+              </v-list-item>
+            </router-link>
+          </v-list-group>
 
         <router-link to="/service-mgmt" style="color: #c8f0f9">
           <v-list-item color="black">
-            <v-list-item-icon class="ml-2 ma-0 mt-2 mr-2">
+            <v-list-item-icon class="ml-4 ma-0 mt-2 mr-2">
               <v-icon small style="color: #e3fff6">mdi-face-agent</v-icon>
             </v-list-item-icon>
-            <v-list-item-content style="color: #e3fff6; font-size: 14px">
+            <v-list-item-content style="color: #e3fff6">
               Service Management
             </v-list-item-content>
           </v-list-item>
@@ -161,10 +134,10 @@
 
         <router-link to="/account-mgmt" style="color: #c8f0f9">
           <v-list-item color="black">
-            <v-list-item-icon class="ml-2 ma-0 mt-2 mr-2">
+            <v-list-item-icon class="ml-4 ma-0 mt-2 mr-2">
               <v-icon small style="color: #e3fff6">mdi-account-multiple</v-icon>
             </v-list-item-icon>
-            <v-list-item-content style="color: #e3fff6; font-size: 14px">
+            <v-list-item-content style="color: #e3fff6">
               Account Management
             </v-list-item-content>
           </v-list-item>
@@ -172,10 +145,10 @@
 
         <router-link to="/report" style="color: #c8f0f9">
           <v-list-item color="black">
-            <v-list-item-icon class="ml-2 ma-0 mt-2 mr-2">
+            <v-list-item-icon class="ml-4 ma-0 mt-2 mr-2">
               <v-icon small style="color: #e3fff6">mdi-account-box</v-icon>
             </v-list-item-icon>
-            <v-list-item-content style="color: #e3fff6; font-size: 14px">
+            <v-list-item-content style="color: #e3fff6">
               Report
             </v-list-item-content>
           </v-list-item>
@@ -189,7 +162,7 @@
           depressed
           small
           rounded
-          color="green"
+          color="#13b150"
           style="color: white; font-size: 12px"
         >
           Logout
@@ -213,8 +186,7 @@ export default {
 
 <style scoped>
 .list-item {
-  font-size: 13px;
-  color: #e3fff6;
+  font-size: 15px;
 }
 a {
   text-decoration: none;
@@ -222,6 +194,12 @@ a {
 >>> .v-list-item__icon.v-list-group__header__append-icon {
   margin-left: 0px !important;
 }
+/* >>>.v-list-item.v-item--active.v-list-item--active.v-list-item--link{
+  color: white !important;
+  background: #0f2e23 !important; 
+  background: #068a41e5 !important; 
+  background: #0B632D !important; 
+} */
 .not-rounded{
   border-radius: 0px !important;
 }
