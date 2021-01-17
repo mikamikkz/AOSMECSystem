202d956cb92db3f23e55fce071f74ce0f688f46c
julcbn <chiongbianpsn@gmail.com>
2021-01-17 22:57:40 +0800

    jl: added a template for room management

--- aosmec/src/router/index.js
+++ aosmec/src/router/index.js
@@ -1,11 +1,13 @@
 import dashboard from '../views/Dashboard'
 import reservation from '../views/Reservation';
 import checkin from '../views/Checkin';
+import roommanagement from '../views/RoomManagement';
 
 export default[
     {path:'/dashboard', component: dashboard},
     {path:'/reservation', component: reservation},
     {path:'/checkin', component: checkin},
+    {path:'/room-mgmt',component: roommanagement}
 ]
 
 
