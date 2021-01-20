5a005468ba388e9618328b69e6c2856f98534840
julcbn <chiongbianpsn@gmail.com>
2021-01-20 21:12:49 +0800

    jl: followed jennen's suggestion for design and made the data appear in console

--- aosmec/src/views/RoomManagement.vue
+++ aosmec/src/views/RoomManagement.vue
@@ -1,155 +1,185 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="room_mgmt"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar 
-        style="background: #13b150"
-        flat
-      >
-        <v-toolbar-title 
-          class="white--text py-3"
-          color="light-green white--text font-weight-bold"
-          style="font-size: 16px"
-        >Manage Rooms</v-toolbar-title>
-        
-        <v-divider
-          class="mx-4"
-          inset
-          vertical
-        ></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog 
-          v-model="dialog"
-          max-width="500px"
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="room_mgmt"
+      class="mt-3"
+      rounded
+    >
+      <template v-slot:top>
+        <v-toolbar 
+          style="background: #13b150"
+          flat
         >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="white"
-              rounded
-              class="black--text py-3"
-              v-bind="attrs"
-              v-on="on"
-            >
-              Add Room
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.roomname"
-                      label="Room Name"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.no_of_rooms"
-                      label="No. of Rooms"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.no_of_adults"
-                      label="No. of Adults"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.additional_head_rate"
-                      label="Additional Head Rate"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
-                    <v-text-field
-                      v-model="editedItem.unit_price"
-                      label="Unit Price"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
+          <v-toolbar-title 
+            class="white--text py-3"
+            color="light-green white--text font-weight-bold"
+            style="font-size: 16px"
+          >Manage Rooms</v-toolbar-title>
+          
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          ></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog 
+            v-model="dialog"
+            max-width="500px"
+          >
+            <template v-slot:activator="{ on, attrs }">
               <v-btn
-                color="green darken-1"
-                text
-                @click="close"
+                color="white"
+                rounded
+                class="black--text py-3"
+                v-bind="attrs"
+                v-on="on"
               >
-                Cancel
+                Add Room
               </v-btn>
-              <v-btn
-                color="green darken-1"
-                text
-                @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title fixed-header><v-icon large color="red lighten-1" class="mr-4">mdi-alert</v-icon>Delete Room</v-card-title>
-            <v-card-text>Are you sure you want to delete this room? This action cannot be undone and you will be unable to recover any data</v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="green darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="green darken-1" text @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:[`item.actions`]="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        color="green"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        color="green"
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
-    </template>
-  </v-data-table>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-select
+                      v-bind:items="room_types"
+                      v-model="editedItem.room_types"
+                      item-text="text"
+                      item-value="value"
+                      label="Room Type"
+                      outlined
+                      color="green"
+                  ></v-select>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.no_of_rooms"
+                        label="No. of Rooms"
+                        outlined
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.no_of_adults"
+                        label="No. of Adults"
+                        outlined
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.additional_head_rate"
+                        label="Additional Head Rate"
+                        outlined
+                      ></v-text-field>
+                    </v-col>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.unit_price"
+                        label="Unit Price"
+                        outlined
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="green darken-1"
+                  text
+                  @click="close"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="green darken-1"
+                  text
+                  @click="save"
+                  
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="500px">
+            <v-card>
+              <v-card-title fixed-header><v-icon large color="red lighten-1" class="mr-4">mdi-alert</v-icon>Delete Room</v-card-title>
+              <v-card-text>Are you sure you want to delete this room? This action cannot be undone and you will be unable to recover any data.</v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green darken-1" text @click="closeDelete">Cancel</v-btn>
+                <v-btn color="green darken-1" text @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <v-icon
+          small
+          class="mr-2"
+          color="green"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          small
+          color="green"
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+    </v-data-table>
+  </v-container>
 </template>
+
+<style>
+
+.v-data-table__expanded.v-data-table__expanded__content {
+  box-shadow: none !important;
+  background: #f8f8f8;
+}
+
+.v-data-table thead span {
+  font-weight: bolder;
+  font-size: 13px;
+}
+
+
+</style>
+
+
 
 <script>
   export default {
@@ -158,10 +188,10 @@
       dialogDelete: false,
       headers: [
         {
-          text: 'Room Name',
+          text: 'Room Type',
           align: 'start',
           sortable: false,
-          value: 'roomname',
+          value: 'room_types',
         },
         { text: 'No. of Rooms', value: 'no_of_rooms', sortable: false},
         { text: 'No. of Adults', value: 'no_of_adults' },
@@ -170,16 +200,24 @@
         { text: 'Actions', value: 'actions'},
       ],
       room_mgmt: [],
+
+
+      room_types: [
+        { text: "Single", value: "Single" },
+        { text: "Double", value: "Double" },
+      ],
+
+
       editedIndex: -1,
       editedItem: {
-        roomname: '',
+        room_types: '',
         no_of_rooms: 0,
         no_of_adults: 0,
         additional_head_rate: 0.00,
         unit_price: 0,
       },
       defaultItem: {
-        roomname: '',
+        room_types: '',
         no_of_rooms: 0,
         no_of_adults: 0,
         additional_head_rate: 0.00,
@@ -210,7 +248,7 @@
       initialize () {
         this.room_mgmt = [
           {
-            roomname: 'Single',
+            room_types: 'Single',
             no_of_rooms: 2,
             no_of_adults: 2,
             additional_head_rate: 100,
@@ -223,6 +261,7 @@
         this.editedIndex = this.room_mgmt.indexOf(item)
         this.editedItem = Object.assign({}, item)
         this.dialog = true
+        console.log(this.editedItem)
       },
 
       deleteItem (item) {
@@ -234,6 +273,7 @@
       deleteItemConfirm () {
         this.room_mgmt.splice(this.editedIndex, 1)
         this.closeDelete()
+        console.log(this.editedItem)
       },
 
       close () {
@@ -258,6 +298,7 @@
         } else {
           this.room_mgmt.push(this.editedItem)
         }
+        console.log(this.editedItem)
         this.close()
       },
     },
